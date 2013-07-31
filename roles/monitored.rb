39e7bf686d844467b056657ea59fb8524f677d05
Piotr Pachalko <piotr.pachalko@tieto.com>
2013-07-31 15:23:08 +0200

    Add: comments

--- roles/monitored.rb
+++ roles/monitored.rb
@@ -1,8 +1,15 @@
-# Role definition
+#
+# Chef role definition 
+# for monitored nodes
+#
+# Copyright (c) 2013 Tieto
+#
+# author: piotr.pachalko@tieto.com
+# 
 name        "monitored"
 description "Node that is monitored by Ganglia"
 
-# We use no-role pattern, actual role is defined in a dedicated cookbook
+# We use no-role pattern, actual role is defined in a dedicated, versioned cookbook
 run_list("recipe[role-monitored]")
 
 # eof
