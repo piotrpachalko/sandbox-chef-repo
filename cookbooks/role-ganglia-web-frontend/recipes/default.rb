a70dd704d083e7b59cc8f90441c693f9d1b4ddf2
Piotr Pachalko <piotr.pachalko@tieto.com>
2013-08-01 10:42:53 +0200

    Ref: role ganglia-web-frontend

--- cookbooks/role-ganglia-web-frontend/recipes/default.rb
+++ cookbooks/role-ganglia-web-frontend/recipes/default.rb
@@ -6,35 +6,39 @@
 #
 # author: piotr.pachalko@tieto.com
 #
-include_recipe "ganglia::web"
+
+# gmond
+node.override[:ganglia][:cluster_name] = 'ganglia-web-frontend'
+node.override[:ganglia][:cluster_id]   = 1
+include_recipe "role-monitored"
+
+# gmetad
 include_recipe "ganglia::gmetad"
-
-# own attributes
-#node.default[:'role-monitored'][:provisioning_if] = "eth1"
-#node.default[:'role-monitored'][:cluster_id]      = 1
-
-# override attributes
-#node.override['ganglia']['cluster_name'] = 'ganglia-web-frontend'
-node.override[:ganglia][:unicast] = false
 
 # override template
 clusters = Hash.new {|h,k| h[k]=[]} # fancy initializer that auto-creates arrays for new hash entries
-search(:node, "role:monitored").each do |n|
+search(:node, "role:base").each do |n|
   name = n[:ganglia][:cluster_name]
   ip   = n[:ganglia][:provisioning_ip]
   port = n[:ganglia][:cluster_id].to_i + 10000
  
   if not ip.nil? then clusters[name] << "#{ip}:#{port}" end
-    
 end
+
+Chef::Log.warn clusters.to_s
 
 begin
   t = resources(:template => "/etc/ganglia/gmetad.conf")
   t.source "gmetad.conf.erb"
   t.cookbook "role-ganglia-web-frontend"
   t.variables({
-  	 :clusters        => clusters
+     :clusters        => clusters
   })
 rescue Chef::Exceptions::ResourceNotFound
   Chef::Log.warn "could not find template '/etc/ganglia/gmetad.conf' to modify"
 end
+
+# web app
+include_recipe "ganglia::web"
+
+# eof
